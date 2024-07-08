da52d4093c8bd122332e2c0ebe9163c85bb6e973
tomohisa847 <tomohisa.tanaka61@icloud.com>
2024-07-09 00:19:04 +0900

    loginとlogoutシステムを構築完了

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from MyDatabase import my_open , my_query , my_close
 import pandas as pd
-
+from flask import Flask, request, render_template, session, redirect, url_for
+import sqlite3
 #Data Source Nameのパラメータを辞書型変数で定義
 dsn = {
     'host' : '172.30.0.10',  #ホスト名(IPアドレス)
@@ -10,15 +11,15 @@ dsn = {
     'database' : 'DBproject' #オープンするデータベース名
 }
 
-from flask import Flask,render_template ,request
 #Flaskのコンストラクタ
 app = Flask(__name__ ,static_folder="static")
-
+app.secret_key = 'your_secret_key'
 #ルーティング定義
 @app.route("/")
 def top():
     dbcon,cur = my_open( **dsn )
-    person_id = 'S260007'
+    if 'person_id' in session:
+        person_id = session['person_id']
     sqlstring = f"""
         SELECT *
         FROM PersonalInfo
@@ -40,10 +41,10 @@ def top():
 def login():
     return render_template( "login.html")
 
-@app.route("/login1")
+@app.route("/login1",methods=["POST"])
 def login1():
     dbcon,cur = my_open( **dsn )
-    person_id = request.form["user"]
+    person_id = request.form["username"]
     password = request.form["password"]
     sqlstring =f"""
         select person_id,pass
@@ -52,10 +53,28 @@ def login1():
         ;
     """
     my_query(sqlstring,cur)
-    dbcon.commit()
-    return render_template( "login.html")
+    recset = cur.fetchall()
+    my_close(dbcon,cur)
+    print(recset)
+    if len(recset)==0:
+        isId='null'
+        isPas='null'
+    else:
+        isId = recset[0]["person_id"]
+        isPas = recset[0]['pass']
+    
+    if isId==person_id and isPas==password:
+        session['person_id'] = person_id
+        return redirect(url_for('top'))
+    else:
+        return render_template("debug.html",
+            debug = 'ユーザーIDとパスワードどちらかが間違っています。'
+        )
 
-
+@app.route("/logout")
+def logout():
+    session.pop('person_id', None)
+    return redirect(url_for('login'))
 
 @app.route("/insertHelth")
 def insertHelth():
